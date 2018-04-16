7b62505857b773e7df9dfd62b5bcfca3fa3564ee
biblik <axel.grimault@gmail.com>
2018-04-16 18:27:20 +0200

    Update windows batchfile

--- benchmark/scriptWINDOWS.bat
+++ benchmark/scriptWINDOWS.bat
@@ -1,13 +1,22 @@
-@del /Q result.txt
-@del /Q err.txt
-
+@del /Q standardOutput.txt
+@del /Q errorOutput.txt
 @echo "Create bin directory"
-@md bin
-
+@md ..\bin
 @echo "Create class files"
-@javac -d "./../bin" ./../src/Main.java
 
-for %%f in (./instances/*.tsp) do (
-	@java -cp "./../bin" tsp.Main %%f -t 60 >> standardOutput.txt 2>> errorOutput.txt
-	@timeout /T 1 /NOBREAK
-)
+#set Path=%Path%;C:\Program Files\Java\jdk-9\bin
+
+@javac -d "./../bin" -sourcepath "./../src" ./../src/tsp/*.java
+
+java -cp ".\..\bin" tsp.Main .\..\instances\d198.tsp -t 60 >> standardOutput.txt 2>> errorOutput.txt
+java -cp ".\..\bin" tsp.Main .\..\instances\d198.tsp -t 60 >> standardOutput.txt 2>> errorOutput.txt
+java -cp ".\..\bin" tsp.Main .\..\instances\d657.tsp -t 60 >> standardOutput.txt 2>> errorOutput.txt
+java -cp ".\..\bin" tsp.Main .\..\instances\eil10.tsp -t 60 >> standardOutput.txt 2>> errorOutput.txt
+java -cp ".\..\bin" tsp.Main .\..\instances\eil101.tsp -t 60 >> standardOutput.txt 2>> errorOutput.txt
+java -cp ".\..\bin" tsp.Main .\..\instances\eil51.tsp -t 60 >> standardOutput.txt 2>> errorOutput.txt
+java -cp ".\..\bin" tsp.Main .\..\instances\kroA100.tsp -t 60 >> standardOutput.txt 2>> errorOutput.txt
+java -cp ".\..\bin" tsp.Main .\..\instances\kroA150.tsp -t 60 >> standardOutput.txt 2>> errorOutput.txt
+java -cp ".\..\bin" tsp.Main .\..\instances\KroA200.tsp -t 60 >> standardOutput.txt 2>> errorOutput.txt
+java -cp ".\..\bin" tsp.Main .\..\instances\lin318.tsp -t 60 >> standardOutput.txt 2>> errorOutput.txt
+java -cp ".\..\bin" tsp.Main .\..\instances\pcb442.tsp -t 60 >> standardOutput.txt 2>> errorOutput.txt
+java -cp ".\..\bin" tsp.Main .\..\instances\rat757.tsp -t 60 >> standardOutput.txt 2>> errorOutput.txt 
